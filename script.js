2a69ecac793cd9fc7d6aed71d9ef27d9b9ec21d4
Justin Strang <Justinstrang100@gmail.com>
2022-01-19 15:05:18 -0500

    Got grid setup

---
+++ script.js
@@ -0,0 +1,24 @@
+/*const containerDIV = document.createElement('div');
+document.body.appendChild(containerDIV);
+const grid = document.createElement('div');
+containerDIV.appendChild(grid);*/
+const container = document.createElement('div');
+document.body.appendChild(container);
+container.className = 'container'
+
+
+genDivs(4)
+function genDivs(v) {
+    let e = container;
+    for (i=0;i<v;i++) {
+        let row = document.createElement('div');
+        row.className = 'row';
+        for(x=1;x<=v;x++) {
+            let cell = document.createElement('div');
+            cell.className = 'cell';
+            cell.innerText = (i*v)+x;
+            row.appendChild(cell);
+        }
+    e.appendChild(row);
+    }
+}
