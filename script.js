f3401f3b75a3891f2215fae802105a0e934c0d67
Justin Strang <Justinstrang100@gmail.com>
2022-01-21 14:02:33 -0500

    Added cell select

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ btn.addEventListener('mousedown', function reload() {
     window.location.reload(true);
 })
 btn.addEventListener('mouseup', function input() {
-    let v = prompt();
+    const v = prompt();
     genDivs(v);
+    select(v)
 });
 
 const container = document.createElement('div');
@@ -19,10 +20,54 @@ function genDivs(v) {
         for(x=1;x<=v;x++) {
             let cell = document.createElement('div');
             cell.className = 'cell';
-            /*cell.innerText = (i*v)+x;*/
+           /* cell.innerText = (i*v)+x;*/
             row.appendChild(cell);
         }
     e.appendChild(row);
     }
 }
+
+const color = [,'#0e2431','#fc3a52','#f9b248','#e8d5b7'];
+
+
+function select(v) {
+    const cells = document.querySelectorAll('div.container>div.row>div.cell');
+    let x = cells.length;
+    for(i=0;i<x;i++) {
+        cells[i].addEventListener('mouseover', function color() {
+            cells.style.backgroundColor = 'blue';   
+        })
+    }
+}
+
+/*for (i=0;i<x;i++) {
+    cells[i].addEventListener('mouseover', function color(){
+        cells[i].style.backgroundColor = 'blue';
+    })
+}*/
+
+
+
+/*const contain = document.getElementsByClassName('container');
+const rows = document.querySelectorAll('div.container>div.row');
+console.log(rows[1])*/
+
+
+/*const cell = document.querySelectorAll(".container>.row");
+console.log(cell[0]);
+cell.addEventListener('onmouseover', function(){
+   console.log('boobs')
+   
+   
+    /*for (i=0;i<v;i++) {
+        cell.style.cssText= 'background-color:blue';
+    }*/
+
+
+
+
+/*cell.addEventListener('mouseover', function(){
+    cell.style.backgroundColor = 'red';
+})*/
+
 
